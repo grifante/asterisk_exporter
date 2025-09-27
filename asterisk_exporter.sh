187d15e1b29e29a7a745070b6f831b8c1f7b1604
Giovani <grifante@gmail.com>
2025-09-27 10:23:29 -0300

    Refactoring

--- asterisk_exporter.sh
+++ asterisk_exporter.sh
@@ -4,9 +4,35 @@ SCRAPE_INTERVAL=10
 METRIC_FILE="/opt/prometheus/custom-metrics/asterisk.prom"
 TEMP_FILE=$(mktemp)
 
+process_labels() {
+	labels="{"
+
+  while [ $# -gt 1 ]; do
+    name="$1"
+    value="$2"
+    labels+="$name=\"$value\"," 
+    shift 2
+  done
+
+	labels="${labels%,}"
+	labels+="}"
+
+	echo "$labels"
+}
+
+append_gauge_header () {
+  local metric="$1"
+  echo "# TYPE $metric gauge" >> $TEMP_FILE
+}
+
 append_gauge () {
-  echo "# TYPE $1 gauge" >> $TEMP_FILE
-  echo "$1 $2" >> $TEMP_FILE
+  local metric="$1"
+  local value="$2"
+
+  shift 2
+	local labels=$(process_labels "$@")
+
+  echo "$metric$labels $value" >> $TEMP_FILE
 }
 
 append_counter() {
@@ -31,17 +57,31 @@ collect () {
   AST_ACTIVE_CALLS=$(echo $x | cut -d' ' -f4)
   AST_CALLS_PROCESSED=$(echo $x | cut -d' ' -f7)
 
+  append_gauge_header "asterisk_active_channels"
   append_gauge "asterisk_active_channels" $AST_ACTIVE_CHANNELS
+
+  append_gauge_header "asterisk_active_calls"
   append_gauge "asterisk_active_calls" $AST_ACTIVE_CALLS
+  
   append_counter "asterisk_calls_processed" $AST_CALLS_PROCESSED
 
-  AST_THREADS=$(asterisk -rx 'core show threads' | tail -1 | cut -d' ' -f1)
-  append_gauge "asterisk_core_threads" $AST_THREADS
+  ### asterisk_core_threads 
+  append_gauge_header "asterisk_core_threads"
+
+  AST_THREADS=$(asterisk -rx 'core show threads' | head -n -1 | cut -d' ' -f3 | sort | uniq -c | awk '{$1=$1; print $0}')
+  while IFS= read -r line; do
+    count=$(echo "$line" | awk '{print $1}')
+    thread=$(echo "$line" | awk '{$1=""; sub(/^ +/, ""); print}')
+    append_gauge "asterisk_core_threads" "$count" "thread" "$thread"
+  done <<< "$AST_THREADS"
+  ###
 
   AST_EXTEN_UNKNOWN_STATUS=$(asterisk -rx 'sip show peers' | grep -P '^\d{3,}.*UNKNOWN\s' | wc -l)
+  append_gauge_header "asterisk_exten_status_unknown"
   append_gauge "asterisk_exten_status_unknown" $AST_EXTEN_UNKNOWN_STATUS
 
   AST_EXTEN_QUALIFIED_STATUS=$(asterisk -rx 'sip show peers' | grep -P '^\d{3,}.*OK\s\(\d+' | wc -l)
+  append_gauge_header  "asterisk_exten_status_qualified"
   append_gauge  "asterisk_exten_status_qualified" $AST_EXTEN_QUALIFIED_STATUS
 }
 
